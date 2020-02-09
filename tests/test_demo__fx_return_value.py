8426d65c75c42d17ba45004bf1d66ccfa32bd20b
namgivu <nam.gi.vu@gigacover.com>
2020-02-09 16:33:38 +0700

    use plain test class type here ref. https://stackoverflow.com/q/60128057/248616

--- tests/test_demo__fx_return_value.py
+++ tests/test_demo__fx_return_value.py
@@ -12,7 +12,8 @@ def test0(fx_return_value):
     print(fx_return_value)
 
 
-class Test1(TestCase):
+class Test1:  # NOTE: must use plain test class type here ref. https://stackoverflow.com/q/60128057/248616
+
     @pytest.mark.usefixtures(fx_return_value.__name__)
     def test1(self, fx_return_value):  #TODO Error here > TypeError: test1() missing 1 required positional argument: 'fx_return_value'
         print(fx_return_value)
